56be06fca062114f36dee80a7a69fe1da6a532f4
sameyr <ss1366t@gre.ac.uk>
2023-11-28 12:24:15 +0000

    changes made

--- node.js/databse.js
+++ node.js/databse.js
@@ -2,13 +2,13 @@ var mysql      = require('mysql2');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Nottheend321',
+  password : '',
   database : 'try'
 });
  
 connection.connect();
  
-connection.query('SELECT timestamp from sampleinputdata', function (error, results, fields) {
+connection.query('SELECT * from sampleinputdata', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results);
 });
