be0648842e0677821940b0ef52df2aa00541b0a3
sameyr <ss1366t@gre.ac.uk>
2023-12-27 15:27:24 +0000

    sucessfully fetched data from database

--- php/home.php
+++ php/home.php
@@ -1,20 +1,20 @@
 <?php
     
-    print_r($_POST["timestamp"]);
    if (empty($_POST["timestamp"])){
         die("Timestamp required");
    }
 
    $mysqli = require __DIR__ ."/database.php";
 
-   $sql = sprintf("SELECT * FROM sampleinputdata 
+   $sql = sprintf("SELECT data_Fuel_manifold_Pressure FROM sampleinputdata 
                 WHERE timestamp = '%s';", 
             $mysqli -> real_escape_string($_POST["timestamp"]));
 
    $result = $mysqli -> query($sql);
 
-   $data = $result ->fetch_assoc();
-
-   var_dump($data);
+   while($row = $result->fetch_assoc()) 
+        { 
+            echo "data_Fuel_manifold_Pressure: " . $row["data_Fuel_manifold_Pressure"] . "<br>";
+        } 
 
 ?>
