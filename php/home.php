e909a90cbb656a59fa2557b9a756e410375bc25e
sameyr <ss1366t@gre.ac.uk>
2023-12-28 22:26:02 +0000

    Data visualisation -1

--- php/home.php
+++ php/home.php
@@ -33,7 +33,8 @@
     <script>
         // Assuming PHP echoes the data in JSON format
         var rawData = <?php echo json_encode($dataFuelMainfold); ?>;
-
+        
+        
         // Extracting the pressure values from the nested array
         var data_Fuel_manifold_Pressure = rawData.map(function(d) {
             return d.data_Fuel_manifold_Pressure;
@@ -41,13 +42,22 @@
 
         var container = d3.select("body")
             .append("svg")
-            .attr("height", 200)
+            .attr("height", 500)
             .attr("width", 700)
             .append("g")
             .attr("transform", "translate(100,100)");
 
-        var xScale = d3.scaleLinear().domain([0, data_Fuel_manifold_Pressure.length - 1]).range([0, 500]);
-        var yScale = d3.scaleLinear().domain([d3.min(data_Fuel_manifold_Pressure), d3.max(data_Fuel_manifold_Pressure)]).range([100, 0]);
+        var xScale = d3.scaleLinear()
+                        .domain([0, data_Fuel_manifold_Pressure.length - 1])
+                        .range([0, 500]);
+
+        var yScale = d3.scaleLinear()
+                        .domain([d3.min(data_Fuel_manifold_Pressure), d3.max(data_Fuel_manifold_Pressure)])
+                        .range([100, 0]);
+
+
+        var scale = d3.scaleLinear().domain([0,60]).range([0,500]);
+        var axis = d3.axisBottom(scale);
 
         var line = container.selectAll("line")
             .data(data_Fuel_manifold_Pressure)
@@ -55,10 +65,14 @@
             .append("line")
             .attr("x1", function (d, i) { return xScale(i); })
             .attr("y1", function (d) { return yScale(d); })
-            .attr("x2", function (d, i) { return xScale(i + 1); }) // Assuming each data point represents a unit of time
+            .attr("x2", function (d, i) { return xScale(i + 1); }) 
             .attr("y2", function (d) { return yScale(d); })
             .attr("stroke", "green");
+
+        container.append("g")
+                    .attr("transform","translate(0,150)")
+                    .call(axis);
+                
     </script>
 </body>
 </html>
-
