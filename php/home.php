42c83a0068d4ad1c9114ee13233a5d46293ee78f
sameyr <ss1366t@gre.ac.uk>
2023-12-28 16:47:02 +0000

    trying to use D3 to visualise the stored fetched data array

--- php/home.php
+++ php/home.php
@@ -23,3 +23,29 @@
     }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src ="http://d3js.org/d3.v4.js"></script>
+    </head>
+    <body>
+        <script>
+            var container = d3.select("body")
+                                .append("svg")
+                                .attr("height",700)
+                                .attr("width",700)
+                                .append("g")
+                                .attr("transform","translate(100,100)");
+
+            var line =container.selectALL("line")
+                                .data(data_Fuel_manifold_Pressure)
+                                .enter()
+                                    .append("line")
+                                    .attr("x1",0)
+                                    .attr("y1",data_Fuel_manifold_Pressure[0])
+                                    .attr("x2",59)
+                                    .attr("y2",data_Fuel_manifold_Pressure[59])
+                                    .attr("stroke","green");
+        </script>
+    </body>    
