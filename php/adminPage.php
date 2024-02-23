65283fab02012857faa75ed0fac3bd0dd7f74ddb
sameyr <ss1366t@gre.ac.uk>
2024-02-23 22:29:39 +0000

    admin page in progress...

---
+++ php/adminPage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="adminPageStyle.css">
+    <title>Admin Page</title>
+    </head>
+    <body>
+        <div class="header">
+            <p class="welcome-text">WELCOME ADMIN!</p>
+            <button class="signout-btn" onclick="document.location ='login.php'"><b>Sign Out</b></button>
+        </div>
+
+        <div class="sidebar">
+            <p>sidebar</p>
+        </div>
+    <body>
+</html>
