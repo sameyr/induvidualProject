f30ca289ae6aeadfe83456b6a460632242a251ac
sameyr <ss1366t@gre.ac.uk>
2024-02-24 13:31:33 +0000

    admin page in progress... html and css part completed

--- php/adminPage.php
+++ php/adminPage.php
@@ -18,24 +18,67 @@
         </div>
 
         <div class="container">
-            <div class="information-container">
+            <div class="information-container" id ="database">
                 <div class="icon-row">
                     <img class = "info-icon" src ="/databaseIcon.png">
-                    <div class ="info-title">Database</div>
+                </div>
+                <div class ="infoTitleContainer">
+                   <p class= "info-title"> User Database</p>
                 </div>
             </div>
-            <div class="information-container">
-                <div class="icon-row">
-                    <img class = "info-icon" src ="/dataVisualisationIcon.png">
-                    <div class ="info-title">Data Visualisation</div>
+
+            <div class="information-container" id ="dataVisual">
+                <div class="icon-row" >
+                    <img class = "info-icon"  src ="/dataVisualisationIcon.png">
+                </div>
+                <div class ="infoTitleContainer">
+                   <p class= "info-title"> Data Visualisation</p>
                 </div>
             </div>
-            <div class="information-container">
+
+            <div class="information-container" id="settings">
                 <div class="icon-row">
-                    <img class = "info-icon" src ="/settingIcon.png">
-                    <div class ="info-title">Settings</div>
+                    <img class = "info-icon"  src ="/settingIcon.png">
+                </div>
+                <div class ="infoTitleContainer">
+                   <p class= "info-title"> Settings</p>
+                </div>
+            </div>
+
+            <div class="information-container" id="register">
+                <div class="icon-row">
+                    <img class = "info-icon" src ="/registration.png">
+                </div>
+                <div class ="infoTitleContainer">
+                   <p class= "info-title"> Registration</p>
                 </div>
             </div>
         </div>
     <body>
+
+    <script>
+        const database_div = document.getElementById("database");
+
+        database_div.addEventListener('click', function() {
+            console.log('database-div was clicked!');
+        });
+
+        const dataVisual_div = document.getElementById("dataVisual");
+
+        dataVisual_div.addEventListener('click', function() {
+            location.replace("./login.php");
+            console.log('datavisual-div was clicked!');
+        });
+
+        const settings_div = document.getElementById("settings");
+
+        settings_div.addEventListener('click', function() {
+            console.log('settings-div was clicked!');
+        });
+        const register_div = document.getElementById("register");
+
+        register_div.addEventListener('click', function() {
+            console.log('register-div was clicked!');
+        });
+    </script>
 </html>
